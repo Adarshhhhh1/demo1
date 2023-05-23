e898e2425954cee85a27e2c1a0b409cd85d7f1e9
sdac17 <sdac17@sdac17-PC>
2023-05-23 18:48:10 +0530

    main 1

---
+++ Abstract/src/Main.java
@@ -0,0 +1,19 @@
+
+class S{
+	void meth(int a , int b){
+		System.out.println(" A + B = "+(a+b));
+	}
+	void meth(int a , int b, int c){
+		System.out.println(" A + B + C = "+(a+b+c));
+	}
+}
+
+
+public class Main {
+	public static void main(String[] args) {
+		S ob = new S();
+		ob.meth(5, 6);
+		
+	}
+
+}
